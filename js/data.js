e9a9c1243c70efc0c1632ec722d60ac748008694
Liscoder <lizethlopezzamudio@gmail.com>
2016-06-08 07:13:54 -0500

    examen js

---
+++ js/data.js
@@ -0,0 +1,28 @@
+var peru =  [{
+              image:"logocodeacademy.png",
+              name:"Laboratoria Peru",
+            },{
+              image:"raymi.jpg",
+              name:"Raymi Saldamando",
+            },{
+              image:"mariana.jpg",
+              name:"Mariana Costa" ,
+            },{
+              image:"anamaria.jpg",
+              name:"Ana María Martinez Franklin",
+            },{
+              image:"rodulfo.jpg",
+              name:"Rodulfo Prieto",
+            },{
+              image:"andrea.jpg",
+              name:"Andrea Lamas",
+            },{
+              image:"mariapaula.JPG",
+              name:"Maria Paula Rivarola",
+            },{
+              image:"katy.jpg",
+              name:"Katy Sanchez",
+            },{
+              image:"aldo.jpg",
+              name:"Aldo Alfaro",
+            }];
